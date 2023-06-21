a70de67a314d6a5c129d3c67d2f7ad8bd992a562
Gabriel Fogaça <gabrielfogassa@gmail.com>
2023-06-21 02:17:54 -0300

    Inclusão das rotas de autenticação e de inserção e consulta de usuários

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require ('dotenv').config();
 const express = require('express')
 const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const req = require('express/lib/request')
+const db = CyclicDB(process.env.CYCLIC_DB)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -20,7 +23,7 @@ app.use(express.urlencoded({ extended: true }))
 // #############################################################################
 
 // Create or Update an item
-app.post('/:col/:key', async (req, res) => {
+/* app.post('/:col/:key', async (req, res) => {
   console.log(req.body)
 
   const col = req.params.col
@@ -29,36 +32,68 @@ app.post('/:col/:key', async (req, res) => {
   const item = await db.collection(col).set(key, req.body)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
-})
+}) */
 
 // Delete an item
-app.delete('/:col/:key', async (req, res) => {
+/* app.delete('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
   const item = await db.collection(col).delete(key)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
-})
+}) */
 
 // Get a single item
-app.get('/:col/:key', async (req, res) => {
+/* app.get('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
   const item = await db.collection(col).get(key)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
-})
+}) */
 
 // Get a full listing
-app.get('/:col', async (req, res) => {
+/* app.get('/:col', async (req, res) => {
   const col = req.params.col
   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
   const items = await db.collection(col).list()
   console.log(JSON.stringify(items, null, 2))
   res.json(items).end()
-})
+}) */
+
+// Cria ou atualiza um usuário
+app.post('/users/:id', async (req, res) => {
+
+  const id = req.params.id;
+  const item = await db.collection('users').set(id, req.body);
+
+  res.json(item).end();
+});
+
+// Obtem informações de um usuário
+app.get('/users/:id', async (req, res) => {
+  const id = req.params.id;
+  const item = await db.collection('users').get(id);
+
+  res.json(item).end();
+});
+
+// Fluxo de autenticação
+app.get('/auth/:acc_data', async (req, res) => {
+  const accData = req.params.acc_data;
+  let authData = await db.collection('auth').get(accData);
+
+  const allowLogin  = req.body.password == authData.props.password;
+
+  const responseData = {
+    authorized: allowLogin,
+    user_id: allowLogin ? authData.props.user_id : null
+  };
+  console.log(responseData);
+  res.json(responseData).end;
+});
 
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
