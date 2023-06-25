342a71d7e2b96ecac5d70b4ccfd691283dfc96d2
Gabriel Fogaça <gabrielfogassa@gmail.com>
2023-06-25 00:53:32 -0300

    rota de delete

--- index.js
+++ index.js
@@ -101,6 +101,15 @@ app.get('/auth/:acc_data', async (req, res) => {
   res.json(responseData).end;
 });
 
+app.delete('/:col/:key', async (req, res) => {
+  const col = req.params.col
+  const key = req.params.key
+  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+  const item = await db.collection(col).delete(key)
+  console.log(JSON.stringify(item, null, 2))
+  res.json(item).end()
+})
+
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
   res.json({ msg: 'no route handler found' }).end()
