34fd4ba7b06abdd42b4f7d9f28e6551a6083ec85
Gabriel Fogaça <gabrielfogassa@gmail.com>
2023-06-26 00:44:13 -0300

    inserir usuário no fluxo e autenticação

--- index.js
+++ index.js
@@ -8,61 +8,6 @@ const db = CyclicDB(process.env.CYCLIC_DB)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-// var options = {
-//   dotfiles: 'ignore',
-//   etag: false,
-//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-//   index: ['index.html'],
-//   maxAge: '1m',
-//   redirect: false
-// }
-// app.use(express.static('public', options))
-// #############################################################################
-
-// Create or Update an item
-/* app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
-
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-}) */
-
-// Delete an item
-/* app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-}) */
-
-// Get a single item
-/* app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-}) */
-
-// Get a full listing
-/* app.get('/:col', async (req, res) => {
-  const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-  const items = await db.collection(col).list()
-  console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
-}) */
-
 // Cria ou atualiza um usuário
 app.post('/users/:id', async (req, res) => {
 
@@ -101,6 +46,16 @@ app.get('/auth/:acc_data', async (req, res) => {
   res.json(responseData).end;
 });
 
+// Insere um usuário no fluxo de autenticação
+app.post('/auth/:id', async (req, res) => {
+
+  const id = req.params.id;
+  const item = await db.collection('auth').set(id, req.body);
+
+  res.end('ok');
+});
+
+// Rota genérica de delete
 app.delete('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
